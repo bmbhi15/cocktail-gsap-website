65c701ce72547e630c2b981bdd8bb75cec42a65c
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-15 21:18:14 +0530

    Added assets, connstants and structure of the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <main>
+      <Hero />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
   );
 }
