605b5faca8ef1bd763a4a2b8ca014d9bbaedd1ac
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-17 03:37:06 +0530

    Added Art section and made animation configuration changes for tweens in Hero section about section

--- app/components/Art.tsx
+++ app/components/Art.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+"use client";
+import { goodLists, featureLists } from "../utils/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Art = () => {
-  return <div>Art</div>;
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#art",
+        start: "top top",
+        end: "bottom center",
+        scrub: 1.5,
+        pin: true,
+      },
+    });
+
+    tl.to(".will-fade", {
+      opacity: 0,
+      stagger: 0.2,
+    })
+      .to(".cocktail-img", {
+        scale: 1.1,
+        maskPosition: "center",
+        maskSize: "400%",
+        duration: 2,
+        ease: "power1.inOut",
+      })
+      .to(".masked-content", {
+        opacity: 1,
+        duration: 1,
+      });
+  }, []);
+  return (
+    <section id="art">
+      <h2 className="will-fade">The ART</h2>
+      <div className="content w-full px-auto md:px-40">
+        <ul className="flex flex-col will-fade space-y-4">
+          {goodLists.map((item) => (
+            <li key={item} className="flex flex-row space-x-4">
+              <img alt="check-icon" src="/images/check.png" />
+              <p>{item}</p>
+            </li>
+          ))}
+        </ul>
+        <img
+          className="cocktail-img masked-img"
+          src="/images/under-img.jpg"
+          alt="under image"
+        />
+        <ul className="flex flex-col will-fade space-y-4">
+          {featureLists.map((item) => (
+            <li key={item} className="flex flex-row space-x-4">
+              <img alt="check-icon" src="/images/check.png" />
+              <p>{item}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="masked-container mt-10">
+        <h2 className="will-fade">Sip-Worthy Perfection</h2>
+        <div className="masked-content">
+          <h3>Made with Craft, Poured with Passion</h3>
+          <p>
+            This isn't just a drink. It's a carefully crafted moment just for
+            you.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Art;
